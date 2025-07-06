00cb50f1de1d676a0df3b56fe8d84ab749b7af03
kalenderfamily <96819979+kalenderfamily@users.noreply.github.com>
2025-07-06 18:07:54 +0200

    Create info.php

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+//This is the modified old default theme of pluck
+//Designer: Tim Kalender, http://www.kalenderfamily.de
+//You can find pluck at http://www.pluck-cms.org
+
+$themedir = "html5up-astral";
+$themename = "html5up-astral";
+$module_space[0] = "main";
+$module_space[1] = "footer";
+?>
